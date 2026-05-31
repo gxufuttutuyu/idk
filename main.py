3bb9d386ad120c1215ed2d52f8468011a3853754
gxufuttutuyu <arsenkorotinskij@gmail.com>
2026-05-31 14:47:31 +0300

    це вже фін. проєкт!!!

--- main.py
+++ main.py
@@ -1,36 +1,40 @@
+import tkinter as tk
 import requests
 from bs4 import BeautifulSoup
 
-urls = [
-    "https://uk.wikipedia.org/wiki/Python",
-]
 
-search_word = input("Що хочеш знайти в вікіпедії?   ").strip().lower()
+def search():
+    word = entry.get().strip().lower()
+    url = f"https://wikipedia.org{word.replace(' ', '_')}"
 
-headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"}
-
-print("\n--- Починаєм пошук ---")
-
-for url in urls:
     try:
-        response = requests.get(url, headers=headers, timeout=5)
-        response.encoding = 'utf-8'
+        html = requests.get(url, timeout=5).text
+        text = " ".join(BeautifulSoup(html, "html.parser").get_text().split())
 
-        soup = BeautifulSoup(response.text, 'html.parser')
+        sentences = text.split(". ")
 
-        for script in soup(["script", "style"]):
-            script.decompose()
+        found_sentence = None
+        for s in sentences:
+            if word in s.lower():
+                found_sentence = s
+                break
 
-        site_text = soup.get_text().lower()
-
-        preview = " ".join(site_text.split())[:120]
-        print(f"\nСайт відкрито: {url}")
-        print(f"Початок тексту на сайті: \"{preview}...\"")
-
-        if search_word in site_text:
-            print(f"Результат: Знайдено слово '{search_word}':)")
+        if found_sentence:
+            result_label.config(text=f"Знайшов:\n\n{found_sentence}.:)", fg="green")
         else:
-            print(f"Результат: Слово '{search_word}' не знайдено:(")
+            result_label.config(text="Слова не бачу:(", fg="red")
+    except:
+        result_label.config(text="Помилка зв'язку з Вікі:(", fg="red")
 
-    except Exception as error:
-        print(f"\n⚠️ Помилка: немає зв'язку з сайтом {url}: {error}")
+root = tk.Tk()
+root.geometry("400x200")
+
+entry = tk.Entry(root, width=30)
+entry.pack(pady=10)
+
+tk.Button(root, text="Знайти", command=search).pack()
+
+result_label = tk.Label(root, text="", wraplength=380, justify="left")
+result_label.pack(pady=10)
+
+root.mainloop()
